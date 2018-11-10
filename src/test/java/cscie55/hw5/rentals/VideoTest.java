7194165fa398c9df9fe732e7daa28605e1589806
Robert Chang <robccorp@gmail.com>
2018-11-10 11:23:57 -0500

    started video test

--- src/test/java/cscie55/hw5/rentals/VideoTest.java
+++ src/test/java/cscie55/hw5/rentals/VideoTest.java
@@ -1,9 +1,21 @@
 package cscie55.hw5.rentals;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
 public class VideoTest { 
 	@Test
 	public void testVideo() {
+	}
+
+	public void testEmptyTitle() {
+
+		try {
+			Video video = new Video(" ", 2005);
+			fail();
+		} catch (VideoException e) {
+			// Expected
+		}
 	}
 }
