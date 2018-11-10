acc871db75d67a871755ba08144be6164411d2b0
Robert Chang <robccorp@gmail.com>
2018-11-10 11:30:21 -0500

    testing video title, year

--- src/test/java/cscie55/hw5/rentals/VideoTest.java
+++ src/test/java/cscie55/hw5/rentals/VideoTest.java
@@ -9,10 +9,33 @@ public class VideoTest {
 	public void testVideo() {
 	}
 
+	@Test
 	public void testEmptyTitle() {
 
 		try {
-			Video video = new Video(" ", 2005);
+			Video video = new Video("",2005);
+			fail();
+		} catch (VideoException e) {
+			// Expected
+		}
+	}
+
+	@Test
+	public void testTooOldYear() {
+
+		try {
+			Video video = new Video("Big",1899);
+			fail();
+		} catch (VideoException e) {
+			// Expected
+		}
+	}
+
+	@Test
+	public void testTooNewYear() {
+
+		try {
+			Video video = new Video("Big",2020);
 			fail();
 		} catch (VideoException e) {
 			// Expected
