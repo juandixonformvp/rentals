edb9af6e84f9e8c5e416256cadcf43ab1bf7ce5e
Robert Chang <robccorp@gmail.com>
2018-11-10 22:10:28 -0500

    video rental test due date

--- src/test/java/cscie55/hw5/rentals/VideoRentalTest.java
+++ src/test/java/cscie55/hw5/rentals/VideoRentalTest.java
@@ -5,6 +5,8 @@ import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.fail;
+import java.time.LocalDate;
+import java.time.Period;
 
 public class VideoRentalTest {
 	@Test
@@ -14,4 +16,14 @@ public class VideoRentalTest {
 		VideoRental videoRental = new VideoRental(video, account);
 		assertEquals(false, video.isAvailable());
 	}
+
+	@Test
+	public void testDueDate() throws VideoException {
+		Video video = new Video("Moana", 2014);
+		Account account = new Account("Bob", "Lee", "anon@gmail.com");
+		VideoRental videoRental = new VideoRental(video, account);
+		Period period = Period.between( LocalDate.now() , videoRental.getDateDue());
+		assertEquals(14, period.getDays());
+	}
+
 }
