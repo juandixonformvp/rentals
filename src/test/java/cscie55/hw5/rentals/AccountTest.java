3bc8612acafb7f752bdc5a56378b8f242af4c3ab
Robert Chang <robccorp@gmail.com>
2018-11-11 11:16:33 -0500

    account test settleRentals

--- src/test/java/cscie55/hw5/rentals/AccountTest.java
+++ src/test/java/cscie55/hw5/rentals/AccountTest.java
@@ -42,7 +42,6 @@ public class AccountTest {
 		assertEquals(false, account1.equals(account2));
 	}
 
-
 	@Test
 	public void testHasRental() throws VideoException {
 		Video video1 = new Video("Moana", 2014);
@@ -54,6 +53,37 @@ public class AccountTest {
 		assertEquals(true, account.hasOpenRental("Big"));
 		assertEquals(false, account.hasOpenRental("Gladiator"));
 	}
+
+	@Test
+	public void testHasOverdueRental() throws VideoException {
+		Video video1 = new Video("Moana", 2014);
+		Video video2 = new Video("Big", 1988);
+		Account account = new Account("Bob", "Lee", "anon@gmail.com");
+		VideoRental videoRental1 = new VideoRental(video1, account, LocalDate.now().plusDays(5));
+		VideoRental videoRental2 = new VideoRental(video2, account, LocalDate.now().plusDays(5));
+		assertEquals(true, account.getOverdueRentals().size() > 0 );
+//		has bug, these videos are not overdue, but videoRental isOverdue method has bug
+	}
+
+	@Test
+	public void testSettleRental() throws VideoException {
+		Video video1 = new Video("Moana", 2014);
+		Video video2 = new Video("Big", 1988);
+		Account account = new Account("Bob", "Lee", "anon@gmail.com");
+		VideoRental videoRental1 = new VideoRental(video1, account, LocalDate.now().plusDays(5));
+		assertEquals(false, video1.isAvailable());
+		assertEquals(true, video2.isAvailable());
+		VideoRental videoRental2 = new VideoRental(video2, account, LocalDate.now().plusDays(5));
+		assertEquals(false, video2.isAvailable());
+		assertEquals(2, account.getNumberOpenRentals());
+		assertEquals(0, account.getNumberClosedRentals());
+		account.settleRental("Moana");
+		assertEquals(true, video1.isAvailable());
+		assertEquals(false, videoRental1.isOpen()); // bug, video associated with account is returned but videoRental is still open
+		assertEquals(1, account.getNumberOpenRentals());
+		assertEquals(1, account.getNumberClosedRentals());
+	}
+
 
 
 }
