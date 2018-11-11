bb4238bc095b4760cff54b9cd0ff7f097198cd90
Robert Chang <robccorp@gmail.com>
2018-11-10 19:32:21 -0500

    work on account test

--- src/test/java/cscie55/hw5/rentals/AccountTest.java
+++ src/test/java/cscie55/hw5/rentals/AccountTest.java
@@ -1,9 +1,28 @@
 package cscie55.hw5.rentals;
 
 import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 
 public class AccountTest {
 	@Test
 	public void testAccount() {
 	}
+
+	@Test
+	public void testEqualAccount() {
+		Account account1 = new Account("Bob", "", "anon@gmail.com");
+		Account account2 = new Account("Bob", "", "anon@gmail.com");
+		assertEquals(true, account1.equals(account2));
+	}
+
+	@Test
+	public void testEqualFname() {
+		Account account1 = new Account("Bob", "", "anon@gmail.com");
+		Account account2 = new Account("Bobb", "", "anon@gmail.com");
+		assertEquals(false, account1.equals(account2));
+	}
+
+
 }
